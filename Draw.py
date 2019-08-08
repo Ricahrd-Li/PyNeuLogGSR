809870493fc536ac4fb78a0b97b351955e87289c
Zhehao Li <1003449832@qq.com>
2019-08-08 10:58:41 -0500

    Update draw

---
+++ Draw.py
@@ -0,0 +1,40 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+# This function is called periodically from FuncAnimation
+# i is frame
+# Note: data should not be list but a value1!
+def animate(i, xs, ys,data):
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(data)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('GSR diagram')
+    plt.ylabel('GSR Value (μS)')
+
+def draw(data):
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys, data), interval=100)
+    plt.show()
+
+if __name__ == '__main__':
+    # Set up plot to call animate() function periodically
+    data = 0.1
+    draw(data)
---
+++ NeulogProcess.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import Draw
+
+startExpURL = "http://localhost:22002/NeuLogAPI?StartExperiment:[GSR],[1],"
+print("------------------- Neulog -------------------")
+rate = input("Rate:\n\
+        1 = 10000 per second\n\
+        2 = 3000 per second\n\
+        3 = 2000 per second\n\
+        4 = 1000 per second\n\
+        5 = 100 per second\n\
+        6 = 50 per second\n\
+        7 = 20 per second\n\
+        8 = 10 per second\n\
+        :")
+sampleNum = input("sample number:")
+
+# startExpURL = startExpURL + '['+ rate +'],[' + sampleNum + ']'
+
+# print(startExpURL)
+
+# start = input("start exp? [y/n]: ")
+# if(start == "y"):
+#     get = requests.get(url = startExpURL)
+
+# Get data from sensor
+# data = []
+# getSamplesURL = "http://localhost:22002/NeuLogAPI?GetExperimentSamples:[GSR],[1]"
+# data = requests.get(url = getSamplesURL).json()
+# dataList = data["GetExperimentSamples"][0]
+# if(len(dataList) > 0):
+#     dataList.pop(0)
+#     dataList.pop(0)
+for i in range(0, int(sampleNum)):
+    data = requests.get(url = "http://localhost:22002/NeuLogAPI?GetSensorValue:[GSR],[1]").json()
+    # data = data['GetSensorValue']
+    print(data)
+
+# requests.get(url = "http://localhost:22002/NeuLogAPI?StopExperiment")
+
+
+
+
